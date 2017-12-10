5c3d817aafc9581b985ed7e6b08b377f5a92a02d
philip ka sing leung <philip.leung90@gmail.com>
2017-12-10 22:25:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -87,7 +87,6 @@ function placeOrder(cardNumber) {
   if(cardNumber == null){
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
-    var totalCost = ;
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
   }
   return cart.splice(0);
